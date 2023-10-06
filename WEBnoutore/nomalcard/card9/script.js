01f94285c7a5161cb9646a2f8796675df0bc2e99
freecar5656 <131416517+freecar5656@users.noreply.github.com>
2023-10-06 10:52:59 +0900

    Add files via upload

---
+++ WEBnoutore/nomalcard/card9/script.js
@@ -0,0 +1,93 @@
+// script.js
+
+// すべてのカード要素を取得
+const cards = document.querySelectorAll(".flip-card");
+
+// 1 から 20 までの数字を含む配列を作成
+const numbers = Array.from({ length: 20 }, (_, index) => (index + 1).toString().padStart(2, '0'));
+
+// 配列をシャッフルする関数
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// 配列をシャッフル
+shuffleArray(numbers);
+
+// 変数に数字を代入
+const [i1, i2, i3, i4, i5, i6, i7, i8, i9] = numbers;
+
+console.log(i1, i2, i3, i4, i5, i6, i7, i8, i9);
+
+// カードの画像を変更する関数
+function changeCardImage(cardId, imageName) {
+  const cardImage = document.getElementById(cardId);
+  cardImage.src = `../../images/${imageName}.jpg`;
+}
+
+// カードのIDとランダムな画像のファイル名を関連付ける
+const cardImageMap = {
+  "card1": `${i1}`,
+  "card2": `${i2}`,
+  "card3": `${i3}`,
+  "card4": `${i4}`,
+  "card5": `${i5}`,
+  "card6": `${i6}`,
+  "card7": `${i7}`,
+  "card8": `${i8}`,
+  "card9": `${i9}`
+};
+
+// カードに画像を割り当てる
+for (const cardId in cardImageMap) {
+  changeCardImage(cardId, cardImageMap[cardId]);
+}
+
+// すべてのカード要素にクリックイベントリスナーを追加
+cards.forEach(card => {
+  card.addEventListener("click", (e) => {
+    // カードをひっくり返す
+    card.classList.toggle("clicked");
+
+    // デバッグ用メッセージ
+    console.log("カードがクリックされました", e);
+  });
+});
+
+// テスト用のコード
+const cardsTest = document.querySelectorAll(".flip-card");
+
+cardsTest.forEach(card => {
+  card.addEventListener("click", function (e) {
+    // カードがクリックされたことを確認する
+    if (e.target === this) {
+      console.log("カードがクリックされました");
+    }
+  });
+});
+// すべて裏返すボタンのクリックイベントリスナーを追加
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", (e) => {
+    // すべてのカードの状態を「裏向き」に設定
+    cards.forEach(card => {
+        card.classList.remove("clicked");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // ページが読み込まれたらアニメーションを開始
+  const body = document.body;
+  body.style.animationPlayState = "running";
+});
+
+// 「次のワークへ」ボタンのクリックイベントリスナーを追加
+const nextWorkButton = document.querySelector(".next-work-button");
+nextWorkButton.addEventListener("click", (e) => {
+    // すべてのカードの状態を「裏向き」に設定
+    cards.forEach(card => {
+        card.classList.remove("clicked");
+    });
+});
---
+++ WEBnoutore/randomcard/card9/script.js
@@ -0,0 +1,88 @@
+// script.js
+
+// すべてのカード要素を取得
+const cards = document.querySelectorAll(".flip-card");
+
+// 1 から 20 までの数字を含む配列を作成
+const numbersImage = Array.from({ length: 20 }, (_, index) => (index + 1).toString().padStart(2, '0'));
+const numbersImageit = Array.from({ length: 20 }, (_, index) => (index + 1).toString().padStart(2, '0'));
+
+// 配列をシャッフルする関数
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// 配列をシャッフル
+shuffleArray(numbersImage);
+shuffleArray(numbersImageit);
+
+// 変数に数字を代入
+const [i1, i3, i5, i7, i9] = numbersImage;
+const [i2, i4, i6, i8] = numbersImageit;
+
+console.log(i1, i3, i5, i7, i9);
+console.log(i2, i4, i6, i8);
+
+// カードの画像を変更する関数
+function changeCardImage(cardId, imageName, folder) {
+    const cardImage = document.getElementById(cardId);
+    cardImage.src = `../../${folder}/${imageName}.jpg`;
+}
+
+// カードのIDとランダムな画像のファイル名を関連付ける
+const cardImageMap = {
+    "card1": { imageName: `${i1}`, folder: "images" },
+    "card2": { imageName: `${i2}`, folder: "imageit" },
+    "card3": { imageName: `${i3}`, folder: "images" },
+    "card4": { imageName: `${i4}`, folder: "imageit" },
+    "card5": { imageName: `${i5}`, folder: "images" },
+    "card6": { imageName: `${i6}`, folder: "imageit" },
+    "card7": { imageName: `${i7}`, folder: "images" },
+    "card8": { imageName: `${i8}`, folder: "imageit" },
+    "card9": { imageName: `${i9}`, folder: "images" }
+};
+
+// カードに画像を割り当てる
+for (const cardId in cardImageMap) {
+    if (cardId.startsWith("card")) {
+        const { imageName, folder } = cardImageMap[cardId];
+        changeCardImage(cardId, imageName, folder);
+    }
+}
+
+// すべてのカード要素にクリックイベントリスナーを追加
+cards.forEach(card => {
+    card.addEventListener("click", (e) => {
+        // カードをひっくり返す
+        card.classList.toggle("clicked");
+
+        // デバッグ用メッセージ
+        console.log("カードがクリックされました", e);
+    });
+});
+// すべて裏返すボタンのクリックイベントリスナーを追加
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", (e) => {
+    // すべてのカードの状態を「裏向き」に設定
+    cards.forEach(card => {
+        card.classList.remove("clicked");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // ページが読み込まれたらアニメーションを開始
+    const body = document.body;
+    body.style.animationPlayState = "running";
+});
+
+// 「次のワークへ」ボタンのクリックイベントリスナーを追加
+const nextWorkButton = document.querySelector(".next-work-button");
+nextWorkButton.addEventListener("click", (e) => {
+    // すべてのカードの状態を「裏向き」に設定
+    cards.forEach(card => {
+        card.classList.remove("clicked");
+    });
+});
